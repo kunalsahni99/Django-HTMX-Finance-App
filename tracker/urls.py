00989354d115d5f9fee0dbfbe4b2750174b58a74
Kunal <kunal.sahni1999@gmail.com>
2025-06-22 21:11:42 +0530

    added Transactions List link

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,5 +4,5 @@ from tracker import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("transactions/", views.transactions_list, name='trasactions-list')
+    path("transactions/", views.transactions_list, name='transactions-list')
 ]
