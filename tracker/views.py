4ad0bab09b502569a87fb36a8e0b974b9bec8e2f
Kunal <kunal.sahni1999@gmail.com>
2025-06-24 21:02:09 +0530

    Display custom statistics in the transaction list

--- tracker/views.py
+++ tracker/views.py
@@ -13,7 +13,16 @@ def transactions_list(request):
         request.GET,
         queryset=Transaction.objects.filter(user=request.user).select_related('category')
     )
-    context = {'filter': transaction_filter}
+
+    total_income = transaction_filter.qs.get_total_income()
+    total_expenses = transaction_filter.qs.get_total_expenses()
+
+    context = {
+        'filter': transaction_filter,
+        'total_income': total_income,
+        'total_expenses': total_expenses,
+        'net_income': total_income - total_expenses
+    }
     if request.htmx:
         return render(request, 'tracker/partials/transactions_container.html', context)
 
